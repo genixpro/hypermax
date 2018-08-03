0ed66a512abdbee6f3a9ee2f076985cd91466e41
Brad Arsenault <brad@electricbrain.io>
2018-08-03 01:05:53 -0400

    Further improvements to hypermax ui.

--- hypermax/cui.py
+++ hypermax/cui.py
@@ -13,6 +13,8 @@ def launchHypermaxUI(optimizer):
         ('frame_header', 'white', 'dark gray', 'standout'),
         ('frame_shadow', 'black', 'black', 'standout'),
         ('frame_body', 'black', 'light gray', 'standout'),
+        ('tab_buttons', 'white', 'dark red', 'standout'),
+
         ('focus', 'black', 'light gray', 'underline'),
         ('reverse', 'light gray', 'black'),
         ('header', 'white', 'dark red', 'bold'),
@@ -23,6 +25,7 @@ def launchHypermaxUI(optimizer):
         ('bright', 'dark gray', 'light gray', ('bold', 'standout')),
         ('buttn', 'black', 'dark cyan'),
         ('buttnf', 'white', 'dark blue', 'bold'),
+
         ('graph_bg', 'black', 'light gray'),
         ('graph_bar', 'black', 'dark cyan', 'bold'),
         ('graph_label', 'dark cyan', 'light gray', 'bold'),
@@ -42,6 +45,8 @@ def launchHypermaxUI(optimizer):
         return padding
 
     graph = None
+    graphVscale = None
+    graphColumns = None
     status = None
     def makeMainMenu():
         content = [
@@ -58,14 +63,15 @@ def launchHypermaxUI(optimizer):
         return menu
 
     def makeGraphArea():
-        nonlocal graph
+        nonlocal graph, graphVscale, graphColumns
         graph = urwid.BarGraph(attlist=['graph_bg', 'graph_bar'])
         graph.set_data([], top=1)
 
-        labels = [[i, '{:.3f}'.format(i)] for i in numpy.arange(0.0, 1.1, 0.1)]
+        labels = [[i, '{:.3f}'.format(i)] for i in numpy.arange(0.0, 1.0, 0.01)]
         graphVscale = urwid.AttrWrap(urwid.GraphVScale(labels=labels, top=1), 'graph_label')
-        graphArea = urwid.Columns([(10, urwid.Padding(graphVscale, left=3, right=0)), graph])
-        graphFrame = makeMountedFrame(graphArea, 'Rolling Loss')
+        graphColumns = urwid.Columns([(10, urwid.Padding(graphVscale, left=3, right=0)), graph, (10, urwid.Padding(graphVscale, left=1, right=2))])
+
+        graphFrame = makeMountedFrame(graphColumns, 'Rolling Loss')
         return graphFrame
 
     def makeStatusArea():
@@ -73,11 +79,40 @@ def launchHypermaxUI(optimizer):
         status = urwid.Text(markup='')
         return makeMountedFrame(urwid.Filler(status), "Status")
 
-    otherFrame = urwid.Pile([makeGraphArea(), makeStatusArea()])
-    columns = urwid.Columns([makeMainMenu(), otherFrame])
+    trialsList = urwid.SimpleFocusListWalker([])
+    def makeTrialsView():
+        listbox = urwid.ListBox(trialsList)
+        return makeMountedFrame(listbox, 'Trials')
+
+    columns = urwid.Columns([makeMainMenu(), urwid.Filler(urwid.Text(''))])
+
+
+    statusArea = makeStatusArea()
+    graphArea = makeGraphArea()
+    trialsArea = makeTrialsView()
+
+    bottomArea = None
+
+    def showLossGraph(widget):
+        bottomArea.contents[1] = (graphArea, (urwid.WEIGHT, 1))
+
+    def showStatus(widget):
+        bottomArea.contents[1] = (statusArea, (urwid.WEIGHT, 1))
+
+    def showTrials(widget):
+        bottomArea.contents[1] = (trialsArea, (urwid.WEIGHT, 1))
+
+    bottomButtons = urwid.Columns([
+        urwid.Filler(urwid.Padding(urwid.AttrWrap(urwid.Button('Loss', on_press=showLossGraph), 'tab_buttons'), left=1, right=5)),
+        urwid.Filler(urwid.Padding(urwid.AttrWrap(urwid.Button('Status', on_press=showStatus), 'tab_buttons'), left=5, right=5)),
+        urwid.Filler(urwid.Padding(urwid.AttrWrap(urwid.Button('Trials', on_press=showTrials), 'tab_buttons'), left=5, right=1)),
+    ])
+
+    bottomArea = urwid.Pile([(2, bottomButtons), graphArea])
 
     background = urwid.Frame(urwid.Pile([
-        urwid.AttrWrap(columns, 'background')
+        urwid.AttrWrap(columns, 'background'),
+        urwid.AttrWrap(bottomArea, 'background')
     ]))
 
     def unhandled(key):
@@ -86,25 +121,49 @@ def launchHypermaxUI(optimizer):
 
     loop = urwid.MainLoop(background, palette, screen, unhandled_input=unhandled)
 
-    def updateStatus(a,b):
-        statusText = "Completed: " + str(optimizer.completed()) + "/" + str(optimizer.totalTrials)
-        status.set_text(statusText)
+    try:
+        loop.start()
+        while True:
+            loop.draw_screen()
 
-        top = 1
+            loop.screen.set_input_timeouts(0.1)
+            keys, raw = loop.screen.get_input(True)
+            keys = loop.input_filter(keys, raw)
+            if keys:
+                loop.process_input(keys)
 
-        allResults = numpy.array([result['loss'] for result in optimizer.results])
+            statusText = "Completed: " + str(optimizer.completed()) + "/" + str(optimizer.totalTrials)
+            status.set_text(statusText)
+
+            trialsToAdd = len(optimizer.results) - len(trialsList)
+            for result in optimizer.results[-trialsToAdd:]:
+                column = []
+                for key in result.keys():
+                    column.append(urwid.AttrWrap(urwid.Padding(urwid.Text(str(result[key]))), 'body'))
+                trialsList.append(urwid.Columns(column))
+
+            allResults = numpy.array([result['loss'] for result in optimizer.results])
+
+            allResults = [numpy.mean(allResults[max(0, index-10):min(len(allResults)-1, index+10)]) for index in range(0, len(allResults), 1)]
+
+            top = 0
+            data = []
+            for result in allResults[-min(len(allResults), 50):]:
+                data.append([result])
+                top = max(top, result*1.1)
+
+            graph.set_data(data, top)
+
+            if top == 0:
+                top = 1
+            labels = [[i, '{:.3f}'.format(i)] for i in numpy.arange(0.0, top, top/100.0)]
+            graphVscale = urwid.AttrWrap(urwid.GraphVScale(labels=labels, top=top), 'graph_label')
+            graphColumns.contents[0] = (urwid.Padding(graphVscale, left=3, right=0), (urwid.GIVEN, 10, False))
+            graphColumns.contents[2] = (urwid.Padding(graphVscale, left=1, right=2), (urwid.GIVEN, 10, False))
 
 
-        allResults = [numpy.mean(allResults[max(0, index-10):min(len(allResults)-1, index+10)]) for index in range(0, len(allResults), 5)]
-
-        data = []
-        for result in allResults[-min(len(allResults), 20):]:
-            data.append([result])
-
-        graph.set_data(data, 1.0)
-
-        loop.set_alarm_in(1.0, updateStatus)
-
-    loop.set_alarm_in(1.0, updateStatus)
-    loop.run()
+    except urwid.ExitMainLoop:
+        pass
+    finally:
+        loop.stop()
 
