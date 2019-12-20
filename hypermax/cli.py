e10f4f499b219e9e40e6bb4d2c8d8f1ca0638b97
Federico Simonetta <federicosimonetta@zoho.com>
2019-12-20 01:00:10 +0100

    Update cli.py

--- hypermax/cli.py
+++ hypermax/cli.py
@@ -10,7 +10,7 @@ import csv
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Provide configuration options for Hypermax')
-    parser.add_argument('configuration_file', metavar='configuration_file', type=argparse.FileType('rb'), nargs=1, help='The JSON based configuration file which is used to configure the hyper-parameter search.')
+    parser.add_argument('configuration_file', metavar='configuration_file', type=str, nargs=1, help='The JSON based configuration file which is used to configure the hyper-parameter search.')
     parser.add_argument('results_directory', metavar='results_directory', type=str, nargs='?', help='The directory of your existing results to reload and restart from.')
 
     args = parser.parse_args()
